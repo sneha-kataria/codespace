73a2b48b0fd79231101d8ca5a4a481b33f14a47c
Sneha Kataria <snehakataria1244@gmail.com>
2025-06-26 22:18:51 -0700

    fixed the code.

--- algo/linearsearch.c
+++ algo/linearsearch.c
@@ -1,10 +1,13 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     int numbers[] = { 20, 50, 100, 10, 5, 50, 1};
-    int n = get_int("Number: ");
+    int n;
+    printf("Enter an integer number: ");
+
+    // Read the integer from the user
+    scanf("%d", &n);
     for (int i = 0; i < 7; i++)
     {
         if(numbers[i] == n)
